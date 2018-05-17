ce4ea35d4dd2392811963f6b3e7732d62b3223cf
khj <250276857@qq.com>
2018-05-17 16:47:03 +0800

    create fcn

---
+++ models/fcn.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from models.utils import conv2d
+from models.utils import conv2d_transpose
+
+class FCN(object):
+    def __init__(self, name: str, is_train: bool):
+        self._name = name
+        self._is_train = is_train
+        self._reuse = None
+
+    def __call__(self, x):
+        with tf.variable_scope(self._name, reuse=self._reuse):
+            x_1 = conv2d(x, 'Conv1', 32, 3, 2, 'SAME', True, tf.nn.relu,self._is_train )
+            x_2 = tf.nn.avg_pool(x_1, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='pooling1')
+            x_3 = conv2d(x_2, 'Conv2', 64, 3, 2, 'SAME', True, tf.nn.relu, self._is_train)
+            x_4 = tf.nn.avg_pool(x_3, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='pooling2')
+            x_5 = conv2d(x_4, 'Conv3', 128, 3, 2, 'SAME', True, tf.nn.relu, self._is_train)
+            x_6 = conv2d_transpose(x_5, 64, 3, 2, 'SAME', True, tf.nn.relu, self._is_train)
+            x_7 = conv2d(x_6, 'Conv4', 64, 3, 2, 'SAME', True, tf.nn.relu, self._is_train)
+            x_8 = conv2d_transpose(x_7, 32, 3, 2, 'SAME', True, tf.nn.relu, self._is_train)
+
+            # x_9,x_10,x_11 as reg layer
+            x_9 = conv2d(x_8, 'Reg1', 2, 3, 1, 'SAME', True, tf.nn.relu, self._is_train)
+            x_10 = conv2d(x_7, 'Reg2', 2, 3, 1, 'SAME', True, tf.nn.relu, self._is_train)
+            x_11 = conv2d(x_5, 'Reg2', 2, 3, 1, 'SAME', True, tf.nn.relu, self._is_train)
+        if self._reuse is None:
+            self.var_list = tf.get_collection_ref(tf.GraphKeys.GLOBAL_VARIABLES, scope=self._name)
+            self.saver = tf.train.Saver(self.var_list)
+            self._reuse = True
+        return x_9, x_10, x_11
+
+    def save(self, session, checkpoint_path):
+        self.saver.save(session, checkpoint_path)
+
+    def restore(self, session, checkpoint_path):
+        self.saver.restore(session, checkpoint_path)
