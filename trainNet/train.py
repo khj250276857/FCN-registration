fd5f2b40dc940bc6a8b68876330134b069b5046a
khj <250276857@qq.com>
2018-05-27 14:46:14 +0800

    edit train

--- trainNet/train.py
+++ trainNet/train.py
@@ -12,7 +12,7 @@ def train():
         'batch_size': 10,
         'learning_rate': 1e-5,
         'epoch_num': 100,
-        'save_interval': 1000,
+        'save_interval': 2,
         'shuffle_batch': True,
         # trainNet data folder
         'checkpoint_dir': r'E:\training data\running data\checkpoints',
@@ -20,21 +20,21 @@ def train():
     })
 
     #定义验证集和训练集
-    train_x_dir = r'E:\training data\pet_ct_train\normolized_pt'
-    train_y_dir = r'E:\training data\pet_ct_train\resized_ct'
+    train_x_dir = r'E:\training data\pet_ct_registration\normolized_pt_train'
+    train_y_dir = r'E:\training data\pet_ct_registration\resized_ct_train'
     batch_x, batch_y = gen_batches(train_x_dir, train_y_dir, {
         'batch_size': config['batch_size'],
         'image_size': config['image_size'],
         'shuffle_batch': config['shuffle_batch']
     })
-    # valid_x_dir = r''
-    # valid_y_dir = r''
-    # valid_x, valid_y = gen_batches(valid_x_dir, valid_y_dir, {
-    #     'batch_size': config['batch_size'],
-    #     'image_size': config['image_size'],
-    #     'shuffle_batch': config['shuffle_batch']
-    # })
-    # valid_iter_num = len(os.listdir(valid_x_dir)) // config['batch_size']
+    valid_x_dir = r'E:\training data\pet_ct_registration\normolized_pt_validate'
+    valid_y_dir = r'E:\training data\pet_ct_registration\resized_ct_validate'
+    valid_x, valid_y = gen_batches(valid_x_dir, valid_y_dir, {
+        'batch_size': config['batch_size'],
+        'image_size': config['image_size'],
+        'shuffle_batch': config['shuffle_batch']
+    })
+    valid_iter_num = len(os.listdir(valid_x_dir)) // config['batch_size']
     config['iteration_num'] = len(os.listdir(train_x_dir)) // config["batch_size"]
 
     #构建网络
@@ -51,9 +51,20 @@ def train():
         for iter_num in range(config['iteration_num']):
             _bx, _by = sess.run([batch_x, batch_y])
             _loss_train = reg.fit(_bx, _by)
-            _train_L.append(_loss_train)
-            print('[TRAIN] epoch={:>3d}, iter={:>5d}, loss={:.4f}'.format(epoch + 1, iter_num + 1, _loss_train))
+            _train_L.append(_loss_train[0])
+            print('[TRAIN] epoch={:>3d}, iter={:>5d}, loss={:.4f}, loss_1={:.4f}, loss_2={:.4f}, loss_3={:.4f}'
+                  .format(epoch + 1, iter_num + 1, _loss_train[0], _loss_train[1], _loss_train[2], _loss_train[3]))
         print('[TRAIN] epoch={:>3d}, loss={:.4f}..................'.format(epoch + 1, sum(_train_L) / len(_train_L)))
+
+        #放入验证集进行验证
+        # for j in range(valid_iter_num):
+        #     _valid_x, _valid_y = sess.run([valid_x, valid_y])
+        #     reg.deploy(config['temp_dir'], _valid_x, _valid_y, j * config['batch_size'])
+
+        if(epoch + 1) % config['save_interval'] == 0:
+            _valid_x, _valid_y = sess.run([valid_x, valid_y])
+            reg.deploy(config['temp_dir'], _valid_x, _valid_y)
+            reg.save(sess, config['checkpoint_dir'])
 
     coord.request_stop()
     coord.join(threads)
