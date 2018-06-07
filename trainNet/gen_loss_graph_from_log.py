9e87419bcb70159a96e98843fdb6c23812baa6cd
khj <250276857@qq.com>
2018-06-07 16:22:49 +0800

    reediting

--- trainNet/gen_loss_graph_from_log.py
+++ trainNet/gen_loss_graph_from_log.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def gen_loss_graph_from_log():
-    workspace = r"F:\log"
+    workspace = r"E:\training data\running data\log"
     train_log = os.path.join(workspace, "train.log")
     valid_log = os.path.join(workspace, "valid.log")
 
@@ -15,18 +15,24 @@ def gen_loss_graph_from_log():
 
     train_y_text = [_.strip() for _ in train_y_text.split("\n") if _ != ""]
     valid_y_text = [_.strip() for _ in valid_y_text.split("\n") if _ != ""]
-    train_y_list = [float(_.split(",")[-1].split("=")[-1]) for _ in train_y_text]
-    valid_y_list = [float(_.split(",")[-1].split("=")[-1]) for _ in valid_y_text]
+    train_y_list = [float(_.split(",")[1].split("=")[-1]) for _ in train_y_text]
+    valid_y_list1 = [float(_.split(",")[2].split("=")[-1]) for _ in valid_y_text]
+    # todo: remove it
+    valid_y_list2 = [float(_.split(",")[3].split("=")[-1]) for _ in valid_y_text]
+    valid_y_list3 = [float(_.split(",")[4].split("=")[-1]) for _ in valid_y_text]
+
     # train_x_list = np.array(range(len(train_y_list))) * 400
     # valid_x_list = np.array(range(len(valid_y_list))) * 400
     train_x_list = np.array(range(len(train_y_list)))
-    valid_x_list = np.array(range(len(valid_y_list)))
+    valid_x_list = np.array(range(len(valid_y_list1)))
 
     # plt.plot(train_x_list, train_y_list, c="red", label='train_loss')
-    plt.plot(valid_x_list, valid_y_list, c="blue", label="valid_loss")
+    plt.plot(valid_x_list, valid_y_list1, c="red", label="valid_loss_1")
+    plt.plot(valid_x_list, valid_y_list2, c="blue", label="valid_loss_2")
+    plt.plot(valid_x_list, valid_y_list3, c="green", label="valid_loss_3")
     plt.xlabel("epoch")
     plt.ylabel("loss(-NCC)")
-    plt.axis([0, 30, -1.8, -1.0])
+    # plt.axis([0, 30, -1.8, -1.0])
     plt.legend(bbox_to_anchor=[1, 1])
     plt.show()
 
