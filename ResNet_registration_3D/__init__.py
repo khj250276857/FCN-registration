420de4a831fb007be96c8c55865a8c3fd7cee604
khj <250276857@qq.com>
2018-09-10 11:04:17 +0800

    new add

---
+++ ResNet_registration_3D/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/9 16:12
+# @Author  : Hongjian Kang
+# @File    : __init__.py.py
---
+++ ResNet_registration_3D/model/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/9 16:13
+# @Author  : Hongjian Kang
+# @File    : __init__.py.py
