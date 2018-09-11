087c1c99fe53361a763fcdf0b00ed7cfd90e1504
khj <250276857@qq.com>
2018-09-11 14:00:46 +0800

    new add

---
+++ ResNet_registration_3D/trainNet/config_folder_guard.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/6 21:05
+# @Author  : Hongjian Kang
+# @File    : config_folder_guard.py
+
+import os
+
+def config_folder_guard(config: dict):
+    if not os.path.exists(config["checkpoint_dir"]):
+        os.makedirs(config["checkpoint_dir"])
+    if not os.path.exists(config["temp_dir"]):
+        os.makedirs(config["temp_dir"])
+    if not os.path.exists(config["log_dir"]):
+        os.makedirs(config["log_dir"])
+    return config
