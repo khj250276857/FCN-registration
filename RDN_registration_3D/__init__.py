bf205fd0237b19849f63c4aa20f1a4f8b45e973c
khj <250276857@qq.com>
2018-09-18 16:51:30 +0800

    new add

---
+++ RDN_registration_3D/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/15 21:02
+# @Author  : Hongjian Kang
+# @File    : __init__.py.py
---
+++ RDN_registration_3D/model/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/15 21:03
+# @Author  : Hongjian Kang
+# @File    : __init__.py.py
